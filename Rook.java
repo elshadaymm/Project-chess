d2a0018aba8446c627f041a4f5a814b3a8f1496b
Jeremy Stuart <45022609+jgpstuart@users.noreply.github.com>
2019-02-24 00:16:13 -0700

    Add files via upload
    minor edit to let rook move properly

--- Rook.java
+++ Rook.java
@@ -8,7 +8,7 @@ public class Rook extends Piece{
         boolean valid = false;
         int dx = abs(from.get_x() - to.get_x());
         int dy = abs(from.get_y() - to.get_y());
-        if (dy == 0 && dy != 0) valid = true;
+        if (dx == 0 && dy != 0) valid = true;
         else if (dy == 0 && dx != 0) valid = true;
         return super.is_valid(board, from, to) && valid;
     }
