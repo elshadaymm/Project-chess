1951f4cca16510d1ab8352fecfd2e77db5d759c6
Yifeng Pan <47012821+yifeng-pan@users.noreply.github.com>
2019-03-04 15:21:01 -0700

    Compiles faster
    Now delets all class files
    and recompiels main.java

--- compile.bat
+++ compile.bat
@@ -1,20 +1,3 @@
-javac Constant.java
-javac Cord.java
-javac Converter.java
-
-javac Piece.java
-javac King.java
-javac Queen.java
-javac Rook.java
-javac Bishop.java
-javac Knight.java
-javac Pawn.java
-javac Empty.java
-
-javac Game.java
-javac Player.java
-javac Engine.java
-javac AIRandom.java
-javac AIMinMax.java
+del *.class
 
 javac Main.java
