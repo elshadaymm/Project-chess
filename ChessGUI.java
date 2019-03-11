f3e5b7b5b94a8f9f028d03dd4efaa4e71eaa33cd
jgpstuart <45022609+jgpstuart@users.noreply.github.com>
2019-03-11 11:46:31 -0600

    GridPane layout.  Pieces moving properly. The indents are all wrong Andrew...so sorry about that :P

--- ChessGUI.java
+++ ChessGUI.java
@@ -3,8 +3,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.Label;
@@ -21,138 +23,142 @@ import javafx.event.ActionEvent;
 
 import javax.sound.sampled.AudioFileFormat.Type;
 
-public class ChessGUI extends Application{
+public class Testing extends Application{
 
   private static Game game = new Game();
   private static Player playerWhite = new Human(game);
   private static Player playerBlack = new AIMinMax(game);
   private static int count = 0;
 
-  public static void main(String[] args)
-  {
-     launch(args);
-  }
-
-  //draws the board
-  public void baseBoard(BorderPane b){
-    Rectangle chessBoard = new Rectangle(10,90,580,580);
-      chessBoard.setStroke(Color.BLACK);
-      b.getChildren().add(chessBoard);
-
-      for(int i=0; i<8; i++) {
-        for(int j=0; j<8; j++) {
-          int check_1= i+j;
-          Rectangle square_s = new Rectangle(10+(72.5*j),90+(72.5*i),72.5,72.5);
-          if (check_1%2==0) {
-            square_s.setFill(Color.WHITE);
-          }
-          else {
-            square_s.setFill(Color.SILVER);
-          }
-          b.getChildren().add(square_s);
-        }
-      }
-    }
-
-  //looping through setting the pieces in place
-  public void drawBoard(BorderPane b){
-    GameHelper.printState(game);
-    for(int i = 0; i < game.getRankSize(); i++) {
-      for(int j = 0; j < game.getFileSize(); j++) {
-
-        boolean pieceColor = game.getPiece(i, j).getColor();
-        String picture;
-        switch (game.getPiece(i,j).getType()) {
-          case King:
-            picture = pieceColor ? "/pictures/80/WhiteKing.png" : "/pictures/80/BlackKing.png";
-            break;
-          case Queen:
-            picture = pieceColor ? "/pictures/80/WhiteQueen.png" : "/pictures/80/BlackQueen.png";
-            break;
-          case Rook:
-            picture = pieceColor ? "/pictures/80/WhiteRook.png" : "/pictures/80/BlackRook.png";
-            break;
-          case Bishop:
-            picture = pieceColor ? "/pictures/80/WhiteBishop.png" : "/pictures/80/BlackBishop.png";
-            break;
-          case Knight:
-            picture = pieceColor ? "/pictures/80/WhiteKnight.png" : "/pictures/80/BlackKnight.png";
-            break;
-          case Pawn:
-            picture = pieceColor ? "/pictures/80/WhitePawn.png" : "/pictures/80/BlackPawn.png";
-            break;
-          default:
-            picture = null;
-            break;}
-          if(picture == null) continue;
-          Image pic = new Image(picture);
-          ImageView toPlace = new ImageView(pic);
-          toPlace.setPreserveRatio(true);
-          toPlace.setX(10+(72 * j));
-          toPlace.setY(90+(72 * i));
-          b.getChildren().add(toPlace);
-      }
-    }
-  }
-
-  @Override
+  public static void main(String[] args){
+     launch(args);}
+  
+	 public void drawBoard(GridPane b){
+	    GameHelper.printState(game);
+	    for(int i = game.getRankSize() -1; i >= 0; i--) {
+	      for(int j = game.getFileSize() -1; j >= 0; j--) {
+	
+	        boolean pieceColor = game.getPiece(i, j).getColor();
+	        String picture;
+	        switch (game.getPiece(i,j).getType()) {
+	          case King:
+	            picture = pieceColor ? "/pictures/80/WhiteKing.png" : "/pictures/80/BlackKing.png";
+	            break;
+	          case Queen:
+	            picture = pieceColor ? "/pictures/80/WhiteQueen.png" : "/pictures/80/BlackQueen.png";
+	            break;
+	          case Rook:
+	            picture = pieceColor ? "/pictures/80/WhiteRook.png" : "/pictures/80/BlackRook.png";
+	            break;
+	          case Bishop:
+	            picture = pieceColor ? "/pictures/80/WhiteBishop.png" : "/pictures/80/BlackBishop.png";
+	            break;
+	          case Knight:
+	            picture = pieceColor ? "/pictures/80/WhiteKnight.png" : "/pictures/80/BlackKnight.png";
+	            break;
+	          case Pawn:
+	            picture = pieceColor ? "/pictures/80/WhitePawn.png" : "/pictures/80/BlackPawn.png";
+	            break;
+	          default:
+	            picture = null;
+	            break;}
+	          if(picture == null) continue;
+	          Image pic = new Image(picture);
+	          ImageView toPlace = new ImageView(pic);
+	          toPlace.setPreserveRatio(true);
+	          b.add(toPlace, j, 7 - i, 1, 1);}  //7 - i is what makes the GridPane start from the bottom left and not top left
+	 	    }
+	  }
+	 
+	 public void baseBoard(GridPane board){
+		 for(int i=0; i<8; i++) {
+	        for(int j=0; j<8; j++) {
+	          int check_1= i+j;
+	          Rectangle square_s = new Rectangle(0, 0,80,80);
+	          if (check_1%2==0) {
+	            square_s.setFill(Color.WHITE);} 
+	          else {
+	            square_s.setFill(Color.SILVER);}
+	          board.add(square_s, i, j, 1, 1);}
+	  }
+	 }
+	 
+ 
   public void start(Stage primaryStage) throws Exception{
+	  Pane root = new Pane();
+	  
+	  //VBox for the right side with the info diplays and the input.  The HBox is a inside this VBox.
+	  VBox infoDisplay = new VBox();
+      infoDisplay.getChildren().add(new Label("Currently " + GameHelper.turnToString(game.getWhiteTurn()) + "'s turn."));
+      //whose turn will go here.
+      infoDisplay.getChildren().add(new Label("Turn: " + game.getTurn()));
+      //Turn Number will go here.
+      infoDisplay.getChildren().add(new Label("Fifty-move Rule: " + game.getPeace()));
+      //50 Move will go here
+      infoDisplay.getChildren().add(new Label("FEN: " + GameHelper.toFEN(game)));
+      infoDisplay.setPadding(new Insets(200,100,20,100));
+      infoDisplay.setSpacing(10);
+      infoDisplay.setLayoutX(600);
+      infoDisplay.setLayoutY(100);
+      
+      //Gets put into the VBox as the last element
+      HBox userInput = new HBox();
+      userInput.getChildren().add(new Label("Input Move: "));
+      TextField txtName = new TextField();
+      txtName.setPrefWidth(150);
+      userInput.getChildren().add(txtName);
+      userInput.setPadding(new Insets(0,0,40,50));
+      Button submit = new Button("Submit");
+      userInput.getChildren().add(submit);
+      infoDisplay.getChildren().add(userInput);
+      
+      GridPane board = new GridPane();
+	  board.setLayoutX(25);
+	  board.setLayoutY(25);
+	   
+	  for(int i=0; i<8; i++) {
+	        for(int j=0; j<8; j++) {
+	          int check_1= i+j;
+	          Rectangle square_s = new Rectangle(0, 0,80,80);
+	          if (check_1%2==0) {
+	            square_s.setFill(Color.WHITE);} 
+	          else {
+	            square_s.setFill(Color.SILVER);}
+	          board.add(square_s, i, j, 1, 1);}
+	  }
 
-    BorderPane root = new BorderPane();
-
-    //rightPane: displays info about the game. example whos turn it is.
-    VBox rightPane = new VBox();
-    rightPane.getChildren().add(new Label("Currently " + GameHelper.turnToString(game.getWhiteTurn()) + "'s turn."));
-    //whose turn will go here.
-    rightPane.getChildren().add(new Label("Turn: " + game.getTurn()));
-    //Turn Numberwill go here.
-    rightPane.getChildren().add(new Label("Fifty-move Rule: " + game.getPeace()));
-    //50 Move will go here
-    rightPane.getChildren().add(new Label("FEN: " + GameHelper.toFEN(game)));
-    rightPane.setPadding(new Insets(200,100,20,100));
-    rightPane.setSpacing(10);
-    root.setRight(rightPane);
-
-    //bottomPane displays a text field to collect input from user
-    VBox bottomPane = new VBox();
-    bottomPane.getChildren().add(new Label("Input Move: "));
-    TextField txtName = new TextField();
-    txtName.setPrefWidth(150);
-    bottomPane.getChildren().add(txtName);
-    bottomPane.setPadding(new Insets(0,0,40,50));
-    Button submit = new Button("Submit");
-    bottomPane.getChildren().add(submit);
-    rightPane.getChildren().add(bottomPane);
-
-    //draws the board
-    baseBoard(root);
-    drawBoard(root);
-
-    submit.setOnAction(new EventHandler<ActionEvent>()
-    {
-     @Override
-     public void handle(ActionEvent event)
-     {
-       String moveInput = txtName.getText();
-       if(count%2==0){
-       playerWhite.move(moveInput);
-       baseBoard(root);
-       drawBoard(root);}
-       else if(count%2!=0){
-         playerBlack.move(moveInput);
-         baseBoard(root);
-         drawBoard(root);}
-         count+=count;
-       }
-     }
-   );
-    int h = 700;
-    int w = (int) (h * 1.6);
-    Scene scene = new Scene(root, w, h);
-    primaryStage.setTitle("Chess Game");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-
-
+	  
+	  drawBoard(board);
+	  
+	  //Button Action Handler
+	  submit.setOnAction(new EventHandler<ActionEvent>()
+	    {
+	     @Override
+	     public void handle(ActionEvent event)
+	     {
+	       String moveInput = txtName.getText();
+	       if(count%2==0){
+	       playerWhite.move(moveInput);
+	       baseBoard(board);
+	       drawBoard(board);}
+	       else if(count%2!=0){
+	         playerBlack.move(moveInput);
+	         baseBoard(board);
+	         drawBoard(board);}
+	         count+=count;
+	       }
+	     }
+	   );
+	  
+	  
+      root.getChildren().add(infoDisplay);
+      root.getChildren().add(board);
+	  int h = 700;
+	    int w = (int) (h * 1.6);
+	    Scene scene = new Scene(root, w, h);
+	    primaryStage.setTitle("Chess Game");
+	    primaryStage.setScene(scene);
+	    primaryStage.show();
   }
+  
 }
