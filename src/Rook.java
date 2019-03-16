269f4a233b24b263b1f379b41696ed650c659323
Yifeng Pan <47012821+yifeng-pan@users.noreply.github.com>
2019-03-15 23:40:37 -0600

    Optimized Rook

--- src/Rook.java
+++ src/Rook.java
@@ -15,24 +15,24 @@ public class Rook extends Piece{
 
         Cord from = move.getFrom();
         Cord to = move.getTo();
-        boolean valid = false;
         int dx = Math.abs(from.getFile() - to.getFile());
         int dy = Math.abs(from.getRank() - to.getRank());
-        if (dy == 0 && dx != 0){
+
+        if(dx != 0 && dy != 0) return false;
+
+        if (dy == 0){
             int mod = to.getFile() - from.getFile() > 0? Constant.POSITIVE : Constant.NEGATIVE;
-            valid = true;
             for(int i = 1; i < dx; i++)
                 if(game.getPiece(new Cord(from.getRank(), from.getFile() + i * mod)).getType() != Type.Empty)
-                    valid = false;
+                    return false;
         }
-        else if (dx == 0 && dy != 0){ 
+        else if (dx == 0){ 
             int mod = to.getRank() - from.getRank() > 0? Constant.POSITIVE : Constant.NEGATIVE;
-            valid = true;
             for(int i = 1; i < dy; i++)
                 if(game.getPiece(new Cord(from.getRank() + i * mod, from.getFile())).getType() != Type.Empty)
-                    valid = false;
+                    return false;
         }
-        return valid;
+        return true;
     }
 
     @Override
@@ -49,13 +49,24 @@ public class Rook extends Piece{
     @Override
     public ArrayList<Cord> validMoves(Game game, Cord from){
         ArrayList<Cord> moves = new ArrayList<Cord>();
-        if(game.getPiece(from).getType() == Type.Empty)
-            return moves;
-        
-        for(int i = 0; i < game.getRankSize(); i++)
-            for(int j = 0; j < game.getFileSize(); j++)
-                if(isValid(game, new Move(from, new Cord(i, j))))
-                    moves.add(new Cord(i, j));
+        Cord test;
+
+        for(int i = 0; i < 2; i++)
+            for(int j = 0; j < 2; j++){
+                int modx = i == 0 ? Constant.POSITIVE : Constant.NEGATIVE;
+                int mody = j == 0 ? Constant.POSITIVE : Constant.NEGATIVE;
+
+                for(int k = 1; k < game.getRankSize() || k < game.getFileSize(); k++){
+                    test = new Cord(from.getRank() + (k * modx), from.getFile());
+                    if(isValid(game, new Move(from, test)))
+                        moves.add(test);
+                        
+                    test = new Cord(from.getRank(), from.getFile() + (k * mody));
+                    if(isValid(game, new Move(from, test)))
+                        moves.add(test);
+                }
+            }
+
         return moves;
     }
 
